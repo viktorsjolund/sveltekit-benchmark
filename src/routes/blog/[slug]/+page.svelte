393b663cd82e9214ad008de94a8f266651746c0e
Denni Wernersson <dw222fy@student.lnu.se>
2023-04-14 19:51:09 +0200

    Add blog/[slug] page functionality

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	const { title, date, content } = data;
+</script>
+
+<article class="center flex w-full flex-col items-center p-2 text-slate-800">
+	<h1 class="text-4xl font-bold">{title}</h1>
+	<div class="py-6">{date}</div>
+	<div class="prose flex w-full flex-col items-center pb-20">{@html content}</div>
+</article>
