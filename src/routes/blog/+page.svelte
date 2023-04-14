1a3e5711494f7f99e5c61a1141856fb0010f14ee
Denni Wernersson <dw222fy@student.lnu.se>
2023-04-14 15:14:12 +0200

    Add hello world blog page

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,7 @@
+<div class="flex w-full grow-0 flex-col gap-2 p-2 text-slate-700">
+	<div class="flex flex-col gap-2 rounded-md border border-slate-300 bg-slate-200 p-3">
+		<a class="font-bold" href="url"> title </a>
+		<small>description</small>
+		<small class="font-light">date</small>
+	</div>
+</div>
