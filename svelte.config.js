0373cbb4ebcd4bf25e3e33ec39d60995b2748ed8
Denni Wernersson <dw222fy@student.lnu.se>
2023-04-14 19:49:58 +0200

    Add md preprocessor config

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,21 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	extensions: ['.svelte', '.md'],
 
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md']
+		})
+	],
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ precompress: true })
 	}
 };
 
